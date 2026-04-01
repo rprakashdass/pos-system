38736d5c01751f87cee621a0a09cecf6a8ab0aa1
prakashdass <prakash.dass@increff.com>
2026-04-01 11:06:00 +0530

    update: k8s, dockerfile (pos-frontend), health-controller(pos-backend)

--- pos-backend/src/main/java/com/rprakashdass/possystem/controller/HealthController.java
+++ pos-backend/src/main/java/com/rprakashdass/possystem/controller/HealthController.java
@@ -1,12 +1,15 @@
 package com.rprakashdass.possystem.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
     @GetMapping("/health")
-    public String health() {
-        return "{status: OK}";
+    public Map<String, String> health() {
+        return Map.of("status", "OK");
     }
 }
